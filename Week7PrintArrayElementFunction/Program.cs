293cc12ec555f560b4ae32a01faee807b2260172
KadriGit <kadri.viira@tktk.ee>
2024-11-04 21:21:13 +0200

    final code

--- Week7PrintArrayElementFunction/Program.cs
+++ Week7PrintArrayElementFunction/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] fruitBakset = { "Apples", "Bananas", "Oranges" };
+string[] pets = { "Cats", "Dogs", "Fish", "Hamasters" };
+string[] characters = {"Harry", "Ron" };
+
+PrintArrayElements(fruitBakset);
+PrintArrayElements(pets);
+
+static void PrintArrayElements(string[] someArray)
+{
+    for (int i = 0; i < someArray.Length; i++)
+    {
+        Console.WriteLine($"{i+1}. {someArray[i]}");
+    }
+}
